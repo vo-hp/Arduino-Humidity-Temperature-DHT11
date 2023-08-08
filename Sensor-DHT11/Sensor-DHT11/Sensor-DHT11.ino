b76da68c43839a26c58009f6e6497999dca825a2
vo-hp <vohuuphuc1911@gmail.com>
2023-08-08 18:51:19 +0700

    Update Sensor-DHT11.ino

--- Sensor-DHT11/Sensor-DHT11/Sensor-DHT11.ino
+++ Sensor-DHT11/Sensor-DHT11/Sensor-DHT11.ino
@@ -7,25 +7,42 @@ DFRobot_DHT11 DHT;
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
+const long interval = 1000;
+unsigned long previousReadMillis = 0;
+unsigned long previousLcdMillis = 0;
+unsigned long previousBlinkMillis = 0;
+unsigned long currentMillis;
+unsigned long currentBlinkMillis;
+
 bool wasBlink = false;
+int blinkCount = 0;
+int a = 0;
 
-void lcdBlink() {
-  for (int i = 0; i < 10; i++) {
-    lcd.noBacklight();
-    delay(100);    
-    lcd.backlight();
-    delay(100);
-  }  
-}
+// void lcdBlink() {
+//   currentMillis = millis();
+//   if (currentMillis - previousBlinkMillis >= 100) {
+//     previousBlinkMillis = millis();
+//     if (not wasBlink) {
+//       if (blinkCount <= 20) {
+//         if (a == 0) {
+//           lcd.noBacklight();
+//           a = 1;
+//           Serial.println("tat");
+//         } else {
+//           lcd.backlight();
+//           a = 0;
+//           Serial.println("bat");
+//         }
+//         blinkCount++;
+//         if (blinkCount == 10) {
+//           wasBlink = true;
+//         }
+//       }
+//     }
+//   }
+// }
 
-void setup(){
-  Serial.begin(115200);
-  lcd.init();
-  lcd.backlight();
-}
-
-void loop(){
-  DHT.read(DHT11_PIN);
+void read() {
   lcd.setCursor(0, 1);
   lcd.print("temp:");
   lcd.setCursor(5, 1);
@@ -38,18 +55,50 @@ void loop(){
   Serial.println(DHT.temperature);
   Serial.print("humi: ");
   Serial.println(DHT.humidity);
-  if (DHT.humidity >= 80) {
-    lcd.setCursor(0, 0);
-    lcd.print("warning");
-    if (not wasBlink) {
-      lcdBlink();
-      wasBlink = true;
+}
+
+void setup() {
+  Serial.begin(115200);
+  lcd.init();
+  lcd.backlight();
+}
+
+void loop() {
+  currentMillis = millis();
+  if (currentMillis - previousReadMillis >= interval) {
+    previousReadMillis = millis();
+    DHT.read(DHT11_PIN);
+    read();
+  }
+  if (currentMillis - previousLcdMillis >= 100) {
+    previousLcdMillis = millis();
+    if (DHT.humidity >= 90) {
+      lcd.setCursor(0, 0);
+      lcd.print("warning");
+      // lcdBlink();
+      if (not wasBlink) {
+        if (blinkCount <= 20) {
+          if (a == 0) {
+            lcd.noBacklight();
+            a = 1;
+            Serial.println("tat");
+          } else {
+            lcd.backlight();
+            a = 0;
+            Serial.println("bat");
+          }
+          blinkCount++;
+          if (blinkCount == 20) {
+            wasBlink = true;
+          }
+        }
+      }  
     }  
+    else {
+      lcd.setCursor(0, 0);
+      lcd.print("       ");
+      wasBlink = false; 
+      blinkCount = 0;
+    }
   }
-  else {
-    lcd.setCursor(0, 0);
-    lcd.print("       ");
-    wasBlink = false;
-  }
-  delay(1000);
 }
